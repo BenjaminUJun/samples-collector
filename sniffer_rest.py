9244b75c8223283a5f2c155490731dcc03ff3275
Ubuntu_Topo <yujun_daxia@163.com>
2016-06-20 13:58:08 +0800

    add sniffer_rest.py

---
+++ sniffer_rest.py
@@ -0,0 +1,24 @@
+import web
+from pymongo import MongoClient
+
+urls=(
+    '/samples','list_samples',
+    '/samples/(.*)','get_samples'
+)
+
+app = web.application(urls,globals())
+client= MongoClient('localhost', 27017)
+samples = client.samples
+
+class list_samples:
+    def GET(self):
+        output = samples.sampleinfo.find()
+        return output
+
+class get_samples:
+    def GET(self,ip_src):
+    	output = samples.sampleinfo.find({"ip_src":ip_src})
+        return output
+
+if __name__ == '__main__':
+    app.run()
