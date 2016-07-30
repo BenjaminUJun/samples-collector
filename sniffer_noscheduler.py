db031f24cb92a85cb9eda7e7812505c24d66db10
Ubuntu_Topo <yujun_daxia@163.com>
2016-07-30 22:26:39 +0800

    modify sniffer_noscheduler.py

--- sniffer_noscheduler.py
+++ sniffer_noscheduler.py
@@ -1,14 +1,12 @@
 import pcap as pcap
-import logging
-import datetime
+import time
 from impacket.ImpactDecoder import EthDecoder
 from impacket.ImpactPacket import IP
 from pymongo import MongoClient
 
 dev = "eth1"
-decoder = EthDecoder()
-	
-def read_packet(sh, hdr, data):
+decoder = EthDecoder()	
+def read_packet(db, hdr, data):
 	ether = decoder.decode(data)
 	if ether.get_ether_type() == IP.ethertype:
 		counter = hdr['len']
@@ -19,7 +17,7 @@ def read_packet(sh, hdr, data):
 		ip_src = iphdr.get_ip_src()
 		ip_dst = iphdr.get_ip_dst()
 
-		db.sampleinfo.update_many({'ip_src':ip_src,'ip_dst':ip_dst}, {'$inc': {'bytes':counter,'packets':1}}, True)
+		db.sampleinfo.update_one({'ip_src':ip_src,'ip_dst':ip_dst}, {'$inc': {'bytes':counter,'packets':1}}, True)
 
 		print ip_src + "->" + ip_dst + " length: " + str(counter)
 
@@ -27,14 +25,15 @@ def run_sniffer():
 	try: 
 		pc = pcap.pcap(dev, 1500, 0, 100)
 		pc.setdirection(pcap.PCAP_D_OUT)
-		pc.setnonblock(True)
-		pc.loop(0, read_packet)
+		#pc.setnonblock(True)
+		db = client.samples
+		pc.loop(0, read_packet,db)
 	except KeyboardInterrupt:
 		pc.breakloop()
 
 try:
 	client = MongoClient('localhost', 27017)
-	db = client.samples
+	client.drop_database("samples")
 	print "connection success"
 except:
 	print "connection fails"
